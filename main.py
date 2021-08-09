878d9d62597b9703d86bfd546279d1c72f739d70
aidanjsharp <aidanjsharp@hotmail.com>
2021-08-09 16:22:36 -0400

    Basic Functions

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-# importing the module 
-from pytube import YouTube 
-  
-# where to save 
-SAVE_PATH = "E:/" #to_do 
-  
-# link of the video to be downloaded 
-link="https://www.youtube.com/watch?v=xWOoBJUqlbI"
-  
-try: 
-    # object creation using YouTube
-    # which was imported in the beginning 
-    yt = YouTube(link) 
-except: 
-    print("Connection Error") #to handle exception 
-  
-# filters out all the files with "mp4" extension 
-mp4files = yt.filter('mp4') 
-  
-#to set the name of the file
-yt.set_filename('GeeksforGeeks Video')  
-  
-# get the video with the extension and
-# resolution passed in the get() function 
-d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution) 
-try: 
-    # downloading the video 
-    d_video.download(SAVE_PATH) 
-except: 
-    print("Some Error!") 
-print('Task Completed!') 
+from pytube import YouTube
+
+def main():
+    SAVE_PATH = "C:/Users/Public/Downloads"
+    link = "https://www.youtube.com/watch?v=xWOoBJUqlbI"
+
+    try:
+        yt = YouTube(link)
+    except:
+        print("Connection ERROR.")
+
+    mp4files = yt.streams.filter(progressive=True)
+
+    d_video = yt.streams.get_by_itag(22)
+    try:
+        d_video.download(SAVE_PATH)
+    except:
+        print("Download ERROR.")
+    print('Video is downloaded to ' + SAVE_PATH + " !")
+
+
+
+main()
