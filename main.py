74e505ca84688609954fbf5a0f2497610faeb2d6
aidanjsharp <aidanjsharp@hotmail.com>
2021-08-10 12:21:32 -0400

    Reorganizing functions, importing tkinter

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from pytube import YouTube
+from tkinter import *
+
 
 def main():
     SAVE_PATH = "C:/Users/Public/Downloads"
     link = "https://www.youtube.com/watch?v=xWOoBJUqlbI"
+    YTdnldFunc(SAVE_PATH, link)
 
+def YTdnldFunc(SAVE_PATH, link):
     try:
         yt = YouTube(link)
+        print("Attempting Download...")
     except:
         print("CONNECTION ERROR.")
 
@@ -13,6 +18,7 @@ def main():
 
     d_video = yt.streams.get_by_itag(22)
     try:
+
         d_video.download(SAVE_PATH)
     except:
         print("DOWNLOAD ERROR.")
