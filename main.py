ecf96d1fdfc08183c8505d34fd5a0b2b479444a0
aidanjsharp <aidanjsharp@hotmail.com>
2021-08-10 14:36:20 -0400

    Creating Entry boxes, labels, and button for GUI

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from pytube import YouTube
 from tkinter import *
 
+root = Tk()
 
-def main():
-    SAVE_PATH = "C:/Users/Public/Downloads"
-    link = "https://www.youtube.com/watch?v=xWOoBJUqlbI"
-    YTdnldFunc(SAVE_PATH, link)
 
-def YTdnldFunc(SAVE_PATH, link):
+
+def myClick():
+    myLabel = Label(root, text = "nice")
+    myLabel.pack()
+
+def YTdownFunc(SAVE_PATH, link):
     try:
         yt = YouTube(link)
         print("Attempting Download...")
@@ -15,15 +17,33 @@ def YTdnldFunc(SAVE_PATH, link):
         print("CONNECTION ERROR.")
 
     mp4files = yt.streams.filter(progressive=True)
-
     d_video = yt.streams.get_by_itag(22)
     try:
-
         d_video.download(SAVE_PATH)
     except:
         print("DOWNLOAD ERROR.")
     print('Video is downloaded to ' + SAVE_PATH + " !")
 
+def main():
 
+    FirstLabel = Label(root, text = "Enter the Save Path Here:")
+    FirstLabel.pack()
+    e = Entry(root, width=50, borderwidth=5)
+    e.pack()
+    e.insert(0, "C:/Users/Public/Downloads")
+    SecondLabel = Label(root, text = "Enter the YouTube link Here:")
+    SecondLabel.pack()
+    d = Entry(root, width=50, borderwidth=5)
+    d.pack()
+
+
+    myButton = Button(root, text= "Enter", command = myClick)
+    myButton.pack()
+
+    root.mainloop()
+
+    SAVE_PATH = "C:/Users/Public/Downloads"
+    link = "https://www.youtube.com/watch?v=xWOoBJUqlbI"
+    #YTdownFunc(SAVE_PATH, link)
 
 main()
